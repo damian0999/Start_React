8f69eaf5ab67e4ae628cf3b6fdd472a07b33808a
Damian <labasdamian@gmail.com>
2016-05-23 11:34:35 +0200

    react state

--- App.js
+++ App.js
@@ -2,25 +2,35 @@ import React from 'react'
 
 // class has state
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = { txt: '' }
+  }
+  update(e){
+    this.setState({
+      txt: e.target.value
+    });
+  }
   render(){
     return(
       <div>
-        <h1> {this.props.title} </h1>
-        <p> {this.props.number} </p>
+        <input type='text' onChange={this.update.bind(this)} />
+        <h1> {this.state.txt} </h1>
+        <p> {this.state.txt.length} </p>
       </div>
     );
   }
 }
 
-App.propTypes = {
-  title: React.PropTypes.string,
-  number: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  title: "You should write your title",
-  number: 0
-}
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   number: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   title: "You should write your title",
+//   number: 0
+// }
 
 // without state
 // const App = () => <h1> Hello </h1>
