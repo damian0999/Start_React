28d0fc3312abeb5be7cc205be7fb2d7a6b5ae251
Damian <labasdamian@gmail.com>
2016-05-24 16:52:59 +0200

    Slider RGB - references

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 // class has state
 class App extends React.Component{
   constructor(){
     super();
-    this.state = { txt: '' }
+    this.state = {
+      txt: '',
+      red: 0,
+      green: 0,
+      blue: 0
+     }
     this.update = this.update.bind(this)
+    this.updateTxt = this.updateTxt.bind(this)
   }
-  update(e){
+  update(){
     this.setState({
-      txt: e.target.value
+      //references
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
+    });
+  }
+  updateTxt(e){
+    this.setState({
+      txt: e.target.value,
     });
   }
   render(){
     return (
-      <Wiget txt = {this.state.txt} count = {this.state.txt.length}
-      update = {this.update} />
+      <div>
+        <Wiget txt = {this.state.txt} count = {this.state.txt.length}
+        updateTxt = {this.updateTxt} />
+      <hr />
+        <Slider ref='red' update={this.update}/>
+        {this.state.red}
+        <br />
+        <Slider ref='green' update={this.update}/>
+        {this.state.green}
+        <br />
+        <Slider ref='blue' update={this.update}/>
+        {this.state.blue}
+        <br />
+      </div>
+    );
+  }
+}
+
+class Slider extends React.Component{
+  render(){
+    return(
+      <input type='range' min='0' max='255' onChange={this.props.update} />
     );
   }
 }
@@ -23,13 +58,12 @@ class App extends React.Component{
 const Wiget = (props) => {
   return(
     <div>
-      <input type='text' onChange={props.update} />
+      <input type='text' onChange={props.updateTxt} />
       <h1> {props.txt} </h1>
       <p> {props.count} </p>
     </div>
   );
 }
-
 
 // App.propTypes = {
 //   txt: React.PropTypes.string,
